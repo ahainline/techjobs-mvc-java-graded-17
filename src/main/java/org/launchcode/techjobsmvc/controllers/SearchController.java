5fbf430675410e5c5747bcbecb0f87dff618997c
Ashley Hainline <aetindle@gmail.com>
2023-10-31 17:25:04 -0500

    Task 4, display search results -- all tests passing

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -40,7 +40,7 @@ public class SearchController {
         model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
 
-        return "redirect:/search";
+        return "search";
     }
 
 }
